cb1096503efc8bd802f33fb57c5ab3d6d3c41322
Gusthenrique69 <117784302+Gusthenrique69@users.noreply.github.com>
2024-06-11 23:37:55 -0300

    Create bancoddadosagroinfa.sql

---
+++ bancoddadosagroinfa.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE cadastro_usuarios;
+
+USE cadastro_usuarios;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    CPF VARCHAR(14) NOT NULL,
+    Endereço VARCHAR(45) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    senha VARCHAR(15) NOT NULL
+);
